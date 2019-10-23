b444e21383003fcfe0df5371f60401f297c3f81e
Cherylshire <54291095+Cherylshire@users.noreply.github.com>
2019-10-23 18:02:48 -0500

    Update example_pages_controller.rb

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -1,10 +1,13 @@
-class Api::ExamplePagesController < ApplicationController
-  def hello_action
-    render json: {message: "Josh, you are going way too fast"}
-  end
+class Api::ExamplePagesController < ApplicationController  # namespace Api through out the controller as module, inheritance from AppicationController
+  # def hello_action                                         # it will trigger the routes.rb hello example
+  #   render json: {message: "hello"}
+  # end
 
+  # def byebye_action
+  #   render json: ["b", "a", "s", "s"]
+  # end
   def time_action
-    @current_time = Time.now.strftime("%l:%M %p")
-    render 'time_view.json.jb'
+    current_time = Time.now.strftime(" %A %B %e %Y %l %m %M %z")
+    render json: {message: "what is the current time?", time: current_time }
   end
 end
